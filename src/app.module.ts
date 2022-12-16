cb752813138d43a6e0b23bfa8d4554c03c184cb2
Ailton Sávio <ailtonS758@gmail.com>
2022-12-16 00:41:47 -0300

    feat(db): setup postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
+import Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number(),
+      }),
+    }),
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
